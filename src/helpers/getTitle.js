0f83e2463f063118f400f26210e9de766850e291
yuviDX <44160525+yuviDX@users.noreply.github.com>
2024-12-22 18:36:07 +0000

    Update getTitle.js

--- src/helpers/getTitle.js
+++ src/helpers/getTitle.js
@@ -74,7 +74,6 @@ export default async function getTitle(id) {
       day: props.aboveTheFoldData.releaseDate.day,
       month: props.aboveTheFoldData.releaseDate.month,
       year: props.aboveTheFoldData.releaseDate.year,
-      //endYear: props.aboveTheFoldData.releaseYear.endYear,
       releaseLocation: {
         country: props.mainColumnData.releaseDate?.country?.text,
         cca2: props.mainColumnData.releaseDate?.country?.id,
