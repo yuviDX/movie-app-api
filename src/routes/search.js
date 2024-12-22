9768771f6c441684951c91063bfb848276ead590
yuviDX <44160525+yuviDX@users.noreply.github.com>
2024-12-22 11:05:01 +0000

    Update search.js
    Added rating to search

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { apiRequestJson } from "../helpers/apiRequestRawHtml";
+import getTitle from "../helpers/getTitle"; // Import getTitle for fetching ratings and other details
 
 const search = new Hono();
 
@@ -16,42 +17,53 @@ search.get("/", async (c) => {
 
     let titles = [];
 
-    data.d.forEach((node) => {
-      try {
-        if (!node.qid) return;
-        if (!["movie", "tvSeries", "tvMovie"].includes(node.qid)) return;
+    // Use Promise.all to fetch details in parallel
+    const detailedResults = await Promise.all(
+      data.d.map(async (node) => {
+        try {
+          if (!node.qid) return null;
+          if (!["movie", "tvSeries", "tvMovie"].includes(node.qid)) return null;
 
-        let imageObj = {
-          image: null,
-          image_large: null,
-        };
+          let imageObj = {
+            image: null,
+            image_large: null,
+          };
 
-        if (node.i) {
-          imageObj.image_large = node.i.imageUrl;
+          if (node.i) {
+            imageObj.image_large = node.i.imageUrl;
 
-          try {
-            let width = Math.floor((396 * node.i.width) / node.i.height);
+            try {
+              let width = Math.floor((396 * node.i.width) / node.i.height);
 
-            imageObj.image = node.i.imageUrl.replace(/[.]_.*_[.]/, `._V1_UY396_CR6,0,${width},396_AL_.`);
-          } catch (_) {
-            imageObj.image = imageObj.image_large;
+              imageObj.image = node.i.imageUrl.replace(/[.]_.*_[.]/, `._V1_UY396_CR6,0,${width},396_AL_.`);
+            } catch (_) {
+              imageObj.image = imageObj.image_large;
+            }
           }
-        }
 
-        titles.push({
-          id: node.id,
-          title: node.l,
-          year: node.y,
-          endYear:node.yr,
-          type: node.qid,
-          ...imageObj,
-          api_path: `/title/${node.id}`,
-          imdb: `https://www.imdb.com/title/${node.id}`,
-        });
-      } catch (_) {
-        console.log(_);
-      }
-    });
+          // Fetch detailed information including ratings
+          const details = await getTitle(node.id);
+
+          return {
+            id: node.id,
+            title: node.l,
+            year: node.y,
+            endYear: node.yr,
+            type: node.qid,
+            rating: details.rating,
+            ...imageObj,
+            api_path: `/title/${node.id}`,
+            imdb: `https://www.imdb.com/title/${node.id}`,
+          };
+        } catch (_) {
+          console.log(_);
+          return null;
+        }
+      })
+    );
+
+    // Filter out null results from failed fetches
+    titles = detailedResults.filter((result) => result !== null);
 
     response.message = `Found ${titles.length} titles`;
     response.results = titles;
